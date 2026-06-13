281b55fe45c999329485700e021129f408ec8bba
Admin <antebu27@gmail.com>
2026-06-13 18:56:58 +0300

    fix allowed hosts

--- agency_project/settings.py
+++ agency_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-))gj_qjk=tbyuxwd94ia!q0z9=nhfw88zv^&n826ixarb!*lu#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
