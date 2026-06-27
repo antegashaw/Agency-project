a200dbab9c4b37834889a20a7e9a986d12cfe60b
Admin <antebu27@gmail.com>
2026-06-27 17:55:04 +0300

    fix file structure and urls

--- agency_project/settings.py
+++ agency_project/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
