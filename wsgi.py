9d75ece2afc5bf20774e6b51d40a2c57d332f751
Admin <antebu27@gmail.com>
2026-07-04 17:14:01 +0300

    Final cleanup of paths

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for agency_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# የፕሮጀክቱ ፋይል ያለበትን ቦታ ለDjango እንንገረው
+sys.path.insert(0, os.path.abspath(os.path.dirname(file)))
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agency_project.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 application = get_wsgi_application()
