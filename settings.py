70d7810aa23d9ee2b67862c59157ea92f8259189
Admin <antebu27@gmail.com>
2026-07-07 19:19:09 +0300

    Fix static path

--- settings.py
+++ settings.py
@@ -132,7 +132,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'registration/static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
