18247838f0670c14bc91038e37ff6740afe7129e
Admin <antebu27@gmail.com>
2026-07-12 16:49:50 +0300

    Fix indentation error in models

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,8 @@ import sys
 from django.core.wsgi import get_wsgi_application
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+import os
+from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Metek_Agency') 
 application = get_wsgi_application()
