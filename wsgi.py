4c91902950dfb025b7c5494f6e2082c1b6e0c849
Admin <antebu27@gmail.com>
2026-07-12 17:56:55 +0300

    Fix settings module path in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,5 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'registration.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'registration')
 application = get_wsgi_application()
