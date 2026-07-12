017d3b4fca6e522cf49c8d52452d9d0e52e64ba6
Admin <antebu27@gmail.com>
2026-07-12 17:28:59 +0300

    Update urls.py with correct view names

--- urls.py
+++ urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from registration.views import apply_now, home_page
-
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),      # ይህንን መስመር ጨምር
+    path('apply/', views.apply, name='apply'),
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='home'),
