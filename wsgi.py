9525a93d4c4bfd6359a5640b16fc7292168c706b
Admin <antebu27@gmail.com>
2026-07-12 19:10:07 +0300

    Final emergency fix for Vercel deployment

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,7 @@
 import os
-import sys
-
-# የፕሮጀክትህን ዋና ፎልደር ወደ ሲስተም መንገድ ጨምር
-sys.path.append(os.getcwd())
-
 from django.core.wsgi import get_wsgi_application
 
+# በ settings.py ውስጥ ያለው ስም በትክክል 'settings' መሆኑን አረጋግጥ
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
