d8a70d205a4b070c551d7ae7a6a3c161fd04ade9
Admin <antebu27@gmail.com>
2026-07-12 22:34:14 +0300

    Fix template inheritance

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # በ settings.py ውስጥ ያለው ስም በትክክል 'settings' መሆኑን አረጋግጥ
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MA_አዲስ_ኤጅንሲ.settings')
 
 application = get_wsgi_application()
