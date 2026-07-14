947fa05a740be26fb8a4e1dc020d2c4a7c230a11
Admin <antebu27@gmail.com>
2026-07-14 17:51:37 +0300

    Final fix for wsgi application handler

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
+import os
 from django.core.wsgi import get_wsgi_application
 
-# የፎልደሩ ስም ADDIS_AGENCY ስለሆነ settings የሚለው በውስጡ ነው ያለው
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ADDIS_AGENCY.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
+# ይህ 'application' የሚለው ቃል ነው Vercel የሚፈልገው!
 application = get_wsgi_application()
